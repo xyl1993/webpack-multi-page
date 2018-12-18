ca293b33999cb1e1a43c1e5421b0bf110f548e79
xyl1993 <602165057@qq.com>
2018-12-18 16:45:37 +0800

    init

--- src/global/base.config.js
+++ src/global/base.config.js
@@ -1,13 +1,2 @@
 export const dateFormat = 'YYYY-MM-DD';  //日期格式化
 export const timeFormat = 'YYYY-MM-DD HH:mm:ss';  //24小时日期格式化
-export const companyType=[{
-  name:'超级管理员',
-  value:1
-},{
-  name:'单位',
-  value:2
-},{
-  name:'服务商',
-  value:3
-}];
-export const deviceSource = ['自购','租用','赠与'];
